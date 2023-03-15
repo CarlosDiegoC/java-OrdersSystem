90f237f612e432c67219e0cf88c2641666d63441
Carlos Costa <carlos_diegoh@homail.com>
2023-03-15 11:53:19 -0300

    creating project

---
+++ src/test/java/com/carlosdiegoc/purchasing/system/PurchasingSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.carlosdiegoc.purchasing.system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PurchasingSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
