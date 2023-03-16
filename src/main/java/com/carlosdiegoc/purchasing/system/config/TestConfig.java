61472f72a3d65eff7e0a58c55a8572f1405b1534
Carlos Costa <carlos_diegoh@homail.com>
2023-03-16 11:58:26 -0300

    Payment, one-to-one association

--- src/main/java/com/carlosdiegoc/purchasing/system/config/TestConfig.java
+++ src/main/java/com/carlosdiegoc/purchasing/system/config/TestConfig.java
@@ -11,6 +11,7 @@ import org.springframework.context.annotation.Profile;
 import com.carlosdiegoc.purchasing.system.entities.Category;
 import com.carlosdiegoc.purchasing.system.entities.Order;
 import com.carlosdiegoc.purchasing.system.entities.OrderItem;
+import com.carlosdiegoc.purchasing.system.entities.Payment;
 import com.carlosdiegoc.purchasing.system.entities.Product;
 import com.carlosdiegoc.purchasing.system.entities.User;
 import com.carlosdiegoc.purchasing.system.entities.enums.OrderStatus;
@@ -81,6 +82,10 @@ public class TestConfig implements CommandLineRunner{
 		
 		orderRepository.saveAll(Arrays.asList(o1,o2,o3));
 		orderItemRepository.saveAll(Arrays.asList(oi1,oi2,oi3,oi4));
+		
+		Payment pay1 = new Payment(null, Instant.parse("2019-06-20T21:53:07Z"), o1 );
+		o1.setPayment(pay1);
+		orderRepository.save(o1);
 		
 		
 		
