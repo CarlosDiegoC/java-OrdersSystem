05969f3535f94e833b4a53edf54c0260ca052138
Carlos Costa <carlos_diegoh@homail.com>
2023-03-16 13:38:18 -0300

    Exception handling - findById

--- src/main/java/com/carlosdiegoc/purchasing/system/services/UserService.java
+++ src/main/java/com/carlosdiegoc/purchasing/system/services/UserService.java
@@ -8,6 +8,7 @@ import org.springframework.stereotype.Service;
 
 import com.carlosdiegoc.purchasing.system.entities.User;
 import com.carlosdiegoc.purchasing.system.repositories.UserRepository;
+import com.carlosdiegoc.purchasing.system.services.exceptions.ResourceNotFoundException;
 
 @Service
 public class UserService {
@@ -21,7 +22,7 @@ public class UserService {
 	
 	public User findById(Long id) {
 		Optional<User> users = userRepository.findById(id);
-		return users.get();
+		return users.orElseThrow(() -> new ResourceNotFoundException(id));
 	}
 	
 	public User insert(User user) {
